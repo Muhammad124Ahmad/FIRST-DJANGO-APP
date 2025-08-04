881053bda752fca0a91ac504188e7a5003fa10d6
Muhammad124Ahmad <muhammad510ahmad@gmail.com>
2025-08-04 18:04:44 +0500

    Made twoendpoints for firstapp/date and firstapp also implemented them

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import date
 
-# Create your views here.
+
+def index(request):
+    template = '''<html>
+  "This is your first view
+  </html>"'''
+    return HttpResponse(content=template)
+
+def get_date(request):
+    today=date.today()
+    template=f'''<html>It is {today}</html>'''
+    return HttpResponse(content=template)
+
+
